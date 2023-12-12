4496c79f742d3c4f664e0b4a9c6049a1b21720a5
RhymeMoon <raymundaban@gmail.com>
2023-12-12 17:19:06 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+// let menu = document.getElementById('menu')
+// let btn = menu.getElementsByClassName('nav-link')
+
+// for (i = 0 ; i < btn.length; i++){
+
+//     btn[i].addEventListener("click", function () {
+//         let current = document.getElementsByClassName("active");
+//             current[0].className = current[0].className.replace("active","");
+//             this.className= "active" 
+//     });
+// }
