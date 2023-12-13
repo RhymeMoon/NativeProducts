e57596e1765b22971a40201c4b46a98e2302e00b
RhymeMoon <raymundaban@gmail.com>
2023-12-13 12:05:52 +0800

    second commit

--- config.js
+++ config.js
@@ -1,4 +1,25 @@
+let hamburger = document.getElementById('hamburger')
+let Xbutton = document.getElementById('close')
+let menu = document.getElementById('menu')
 
+ hamburger.addEventListener('click', () => {
+    hamburger.style.display = 'none'
+    Xbutton.style.display = 'block'
+    menu.style.opacity = '1';
+    menu.style.position= 'absolute'
+    menu.style.right= '0'
+    menu.style.top= '40px'
+    menu.style.textAlign= 'center'
+    menu.style.backgroundColor= 'rgba(86, 64, 30, 0.893)'
+    menu.style.boxShadow= '0 0 5px 5px rgba(86, 64, 30, 0.893)'
+ })
+ 
+ Xbutton.addEventListener('click', () => {
+    hamburger.style.display = 'block'
+    Xbutton.style.display = 'none'
+    menu.style.opacity = '0';
+    
+ })
 
 
 
