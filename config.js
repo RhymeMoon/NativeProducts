15897f1a25f4967f0ed3451d263abebe1841cd58
RhymeMoon <raymundaban@gmail.com>
2023-12-14 13:21:03 +0800

    creating contact page

--- config.js
+++ config.js
@@ -21,6 +21,41 @@ Xbutton.addEventListener("click", () => {
   menu.style.display = "none";
 });
 
+//sending email
+    // Password: "81371FDC6625E9B873C985F9B9E240ED557F",
+function sendEmail() {
+  let username = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let enquiry = document.getElementById("message").value;
+
+  let message =
+    "Name: " +
+    username +
+    "<br> Phone Number: " +
+    phone +
+    "<br> Email: " +
+    email +
+    "<br> Message: " +
+    enquiry;
+
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "raymund.ihap@gmail.com",
+    Password: "23D7CBAAEF181856F0DA3F479E7AC71C69DC",
+    To: "info@rhymemoon.site",
+    From: "abanreset91@gmail.com",
+    Subject: "Native Product Enquiry",
+    Body: message,
+  }).then((message) => {
+    if (message == "OK") {
+      swal("Success", "We well contact you shortly!", "success");
+    } else {
+      swal("Error", "You clicked the button!", "error");
+    }
+  });
+}
+
 // let menu = document.getElementById('menu')
 // let btn = menu.getElementsByClassName('nav-link')
 
